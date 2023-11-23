ccb68a7ce0f176b34e85d1e9f00b9b22af17422d
lucasferreira09 <148561715+lucasferreira09@users.noreply.github.com>
2023-11-23 11:52:09 -0300

    Update cifra_de_cesar.py

--- Codificador Mensagens/cifra_de_cesar.py
+++ Codificador Mensagens/cifra_de_cesar.py
@@ -9,7 +9,7 @@ print(
     "                                 \n"
     "_________________________________\n"
     )
-chave = int(input("Qual a chave de codificação? Entre 1-25: "))
+chave = int(input("Qual a chave de codificação? Entre 1-26: "))
 alfa_original = list(string.ascii_lowercase)
 alfa_chave = alfa_original[:chave]
 alfa_cifrado = alfa_original[chave:] + alfa_chave
@@ -18,8 +18,8 @@ msg_codificada = ''
 msg_original = input("Qual mensagem deseja codificar? ").lower().split()
 for letra in msg_original:
     for letra2 in list(letra):
-        posicao_letra = int(alfa_original.index(letra2)+1) + chave
-        codifica_letra = alfa_cifrado[posicao_letra-chave-1]
+        posicao_letra = int(alfa_original.index(letra2))
+        codifica_letra = alfa_cifrado[posicao_letra]
         msg_codificada += codifica_letra
     msg_codificada += ' '
     
